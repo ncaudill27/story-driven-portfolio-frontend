17f135555fe35b6cdc46bd53aa0dd853975bb96d
Nelson Caudill <nelson.a.caudill@gmail.com>
2022-10-12 23:55:14 -0400

    create randomTwo function for useImages

--- src/hooks/use-images.ts
+++ src/hooks/use-images.ts
@@ -13,10 +13,15 @@ function onlyUniqueAndValid(
   )
 }
 
+// grabs two random images after ignoring the first
+function randomTwo(images: SanityImage[]) {
+  return [...images.slice(1)].sort(() => 0.5 - Math.random()).slice(0, 2)
+}
+
 export function useImages(images: SanityImage[]) {
   images = images.filter(onlyUniqueAndValid)
   const hero = images[0]
-  const briefImages = images.slice(1)
+  const briefImages = randomTwo(images)
   const subjectImage = images[1]
   const elementImages = images.slice(2)
 
