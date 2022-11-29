91fa6e1d5343b9c0940fc38c5da0a003ad4fc61c
Nelson Caudill <nelson.a.caudill@gmail.com>
2022-11-29 13:17:01 -0500

    last commit

--- src/templates/carousel.tsx
+++ src/templates/carousel.tsx
@@ -2,12 +2,11 @@ import * as React from "react"
 import styled from "styled-components"
 import { graphql } from "gatsby"
 import { toPlainText } from "@portabletext/react"
-import { NumberParam, useQueryParam } from "use-query-params"
+import { useQueryParam } from "use-query-params"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Pagination } from "swiper"
 import type { HeadFC, PageProps } from "gatsby"
 import type { IProject } from "../types/project"
-import type { ImageProps } from "../components/image"
 
 import SEO from "../components/seo"
 import Image from "../components/image"
@@ -45,31 +44,42 @@ export default function CarouselTemplate({
   const { images: rawImages } = data.pageData
   const { images } = useImages(rawImages)
   const [currentImage, setCurrentImage] = useQueryParam<number | undefined>("0")
-  const height = typeof window !== "undefined" ? window.innerHeight : 0
+  const height = () => (typeof window !== "undefined" ? window.innerHeight : 0)
 
   return (
     <LayoutContainer location={location}>
-      <StyledSwiper
-        modules={[Pagination]}
-        pagination={{ dynamicBullets: true, type: "bullets" }}
-        onSwiper={swiper => console.log(swiper)}
-        centeredSlides
-        loop
-        initialSlide={currentImage}
-        height={height}
-        style={{
-          "--height": height,
-        }}
-      >
-        {images.map(i => (
-          <SwiperSlide>
-            <Image image={i} />
-          </SwiperSlide>
-        ))}
-      </StyledSwiper>
+      <CarouselWrapper>
+        <StyledSwiper
+          modules={[Pagination]}
+          pagination={{ dynamicBullets: true, type: "bullets" }}
+          onSwiper={swiper => console.log(swiper)}
+          centeredSlides
+          loop
+          initialSlide={currentImage}
+          style={{
+            "--height": height(),
+          }}
+        >
+          {images.map(i => (
+            <SwiperSlide>
+              <ImageWrapper>
+                <Image image={i} />
+              </ImageWrapper>
+            </SwiperSlide>
+          ))}
+        </StyledSwiper>
+      </CarouselWrapper>
     </LayoutContainer>
   )
 }
+
+const CarouselWrapper = styled.div`
+  height: inherit;
+  max-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 interface ISwiper {
   style: {
@@ -78,10 +88,24 @@ interface ISwiper {
 }
 const StyledSwiper = styled(Swiper)<ISwiper>`
   height: var(--height);
+  max-height: var(--height);
 
   .swiper-wrapper {
+    height: var(--height);
+    max-height: var(--height);
     align-items: center;
   }
+
+  .swiper-slide {
+    height: var(--height);
+    max-height: var(--height);
+    padding-inline: 50% - 700px / 2;
+  }
+`
+
+const ImageWrapper = styled.div`
+  height: var(--height);
+  max-height: var(--height);
 `
 
 export const query = graphql`
